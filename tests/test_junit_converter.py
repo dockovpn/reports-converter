2586d664cafac176f720ad4deec655c615838d59
Alexander Litvinenko <alekslitvinenk@gmail.com>
2021-03-03 17:49:24 +0200

    Add more tests

--- tests/test_junit_converter.py
+++ tests/test_junit_converter.py
@@ -3,7 +3,7 @@ import json
 
 
 def test_converts_plain_junit_report():
-    with open("tests/resources/plain_report.xml", "r") as report_xml:
+    with open("tests/resources/plain_report_all_pass.xml", "r") as report_xml:
         report_xml_contents = report_xml.read()
 
     junit_converter = JunitConverter()
@@ -12,7 +12,7 @@ def test_converts_plain_junit_report():
     # breakpoint()
     assert len(json_obj["testsuites"]) == 1
     testsuit = json_obj["testsuites"][0]
-    assert testsuit["name"] == "DummyTest"
+    assert testsuit["name"] == "TestSuite"
     assert testsuit["tests"] == "1"
     assert testsuit["errors"] == "0"
     assert testsuit["failures"] == "0"
@@ -20,3 +20,58 @@ def test_converts_plain_junit_report():
     assert len(testsuit["testcases"]) == 1
     testcase = testsuit["testcases"][0]
     assert testcase["passes"] is True
+
+
+def test_converts_plain_junit_report_with_1_failure():
+    with open("tests/resources/plain_report_1_ok_1_faiulre.xml", "r") as report_xml:
+        report_xml_contents = report_xml.read()
+
+    junit_converter = JunitConverter()
+    report_json = junit_converter.convert(report_xml_contents)
+    json_obj = json.loads(report_json)
+    # breakpoint()
+    assert len(json_obj["testsuites"]) == 1
+    testsuit = json_obj["testsuites"][0]
+    assert testsuit["name"] == "TestSuite"
+    assert testsuit["tests"] == "2"
+    assert testsuit["errors"] == "0"
+    assert testsuit["failures"] == "1"
+    assert testsuit["skipped"] == "0"
+    assert len(testsuit["testcases"]) == 2
+    testcase1 = testsuit["testcases"][0]
+    assert testcase1["passes"] is True
+    testcase2 = testsuit["testcases"][1]
+    assert testcase2["passes"] is False
+
+
+def test_converts_merged_junit_report():
+    with open("tests/resources/merged-report_all_pass.xml", "r") as report_xml:
+        report_xml_contents = report_xml.read()
+
+    junit_converter = JunitConverter()
+    report_json = junit_converter.convert(report_xml_contents)
+    json_obj = json.loads(report_json)
+    # breakpoint()
+    assert len(json_obj["testsuites"]) == 2
+
+    testsuit1 = json_obj["testsuites"][0]
+    assert testsuit1["name"] == "TestSuite1"
+    assert testsuit1["tests"] == "2"
+    assert testsuit1["errors"] == "0"
+    assert testsuit1["failures"] == "0"
+    assert testsuit1["skipped"] == "0"
+    assert len(testsuit1["testcases"]) == 2
+    testcase1_1 = testsuit1["testcases"][0]
+    assert testcase1_1["passes"] is True
+    testcase1_2 = testsuit1["testcases"][1]
+    assert testcase1_2["passes"] is True
+
+    testsuit2 = json_obj["testsuites"][1]
+    assert testsuit2["name"] == "TestSuite2"
+    assert testsuit2["tests"] == "1"
+    assert testsuit2["errors"] == "0"
+    assert testsuit2["failures"] == "0"
+    assert testsuit2["skipped"] == "0"
+    assert len(testsuit2["testcases"]) == 1
+    testcase2_1 = testsuit2["testcases"][0]
+    assert testcase2_1["passes"] is True
