56e9cd024e48b94d5709714e2e3b4dbdeb15d14f
Alexander Litvinenko <alekslitvinenk@gmail.com>
2021-03-07 15:02:07 +0200

    Wrap tool as an installable package

--- reportconverter/report_converter.py
+++ reportconverter/report_converter.py
@@ -1,8 +1,28 @@
-class ReportConverter:
+from reportconverter.base_converter import BaseConverter
+from reportconverter.junit_converter import JunitConverter
 
-    def __init__(self, input_file: str, output_file: str):
-        self.input_file = input_file
-        self.output_file = output_file
+junit_format = "junit"
 
-    def convert(self):
-        pass
+converters = {
+    junit_format: JunitConverter()
+}
+
+
+def get_converter(input_format: str):
+    converter: BaseConverter = converters[input_format]
+    return converter
+
+
+def convert_report(input_file_path: str, output_file_path: str, input_format: str):
+    with open(input_file_path, "r") as input_file:
+        xml_str = input_file.read()
+
+    cicd_json_str = convert_report_to_cicd_format(xml_str, input_format)
+
+    with open(output_file_path, "w") as output_file:
+        output_file.write(cicd_json_str)
+
+
+def convert_report_to_cicd_format(json_str: str, input_format: str):
+    converter: BaseConverter = get_converter(input_format)
+    return converter.convert(json_str)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='reportconverter',
+    version='0.1',
+    packages=['reportconverter'],
+    url='www.dockovpn.io',
+    license='MIT',
+    author='Alexander Litvinenko',
+    author_email='array.shift@yahoo.com',
+    description='Utility tool that converts different test report formats',
+    scripts=['bin/report-converter']
+)
