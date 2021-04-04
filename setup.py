84a91096a7650806f63966b7f19b198e02a896a0
Alexander Litvinenko <alekslitvinenk@gmail.com>
2021-04-04 22:16:13 +0300

    Fix testcase name (#4)
    Fixes #3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='reportconverter',
-    version='0.1',
+    version='0.3',
     packages=['reportconverter'],
     url='www.dockovpn.io',
     license='MIT',
